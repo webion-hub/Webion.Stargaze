d49bc1b7642ac2232f252d6e63dd67bd4e18ad3f
Filippo Bellei <filippobellei@gmail.com>
2024-05-16 18:19:36 +0200

    fix(WEBION-128): add pagination constraint

--- Webion.Stargaze.Api/Controllers/Dtos/PaginatedRequest.cs
+++ Webion.Stargaze.Api/Controllers/Dtos/PaginatedRequest.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Webion.Stargaze.Api.Controllers.Dtos;
 
 public abstract class PaginatedRequest
 {
     public int Page { get; init; }
-    public int PageSize { get; init; }
+
+    [Range(1, 100)]
+    public required int PageSize { get; init; }
 }
