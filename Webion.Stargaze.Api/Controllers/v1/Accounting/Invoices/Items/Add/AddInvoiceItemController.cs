31e3a7eba1b28ccfbba8097f9bb0a9334ba3f3b9
Filippo Bellei <filippobellei@gmail.com>
2024-05-17 15:17:40 +0200

    feat(WEBION-91): implement invoice items crud

---
+++ Webion.Stargaze.Api/Controllers/v1/Accounting/Invoices/Items/Add/AddInvoiceItemController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Webion.Stargaze.Pgsql;
+using Webion.Stargaze.Pgsql.Entities.Accounting;
+
+namespace Webion.Stargaze.Api.Controllers.v1.Accounting.Invoices.Items.Add;
+
+[Authorize]
+[ApiController]
+[Route("v{version:apiVersion}/invoices/{invoiceId}/items")]
+[Tags("Invoices")]
+[ApiVersion("1.0")]
+public sealed class AddInvoiceItemController : ControllerBase
+{
+    private readonly StargazeDbContext _db;
+
+    public AddInvoiceItemController(StargazeDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpPost]
+    [ProducesResponseType<AddInvoiceItemResponse>(201)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> Create(
+        [FromRoute] Guid invoiceId,
+        [FromBody] AddInvoiceItemRequest request,
+        CancellationToken cancellationToken
+    )
+    {
+        var invoiceExist = await _db.Invoices
+            .AnyAsync(x => x.Id == invoiceId, cancellationToken);
+
+        if (!invoiceExist)
+            return NotFound();
+
+        var invoiceItem = new InvoiceItemDbo
+        {
+            InvoiceId = invoiceId,
+            TotalUnits = request.TotalUnits,
+            Description = request.Description,
+            NetPrice = request.NetPrice,
+            TaxedPrice = request.NetPrice * (request.VatPercentage / 100 + 1),
+            VatPercentage = request.VatPercentage,
+        };
+
+        _db.InvoiceItems.Add(invoiceItem);
+        await _db.SaveChangesAsync(cancellationToken);
+
+        return Created($"v1/invoices/{invoiceId}/items/{invoiceItem.Id}", new AddInvoiceItemResponse
+        {
+            Id = invoiceItem.Id,
+        });
+    }
+}
--- Webion.Stargaze.Api/Controllers/v1/Accounting/Invoices/Items/GetAll/GetAllInvoiceItemsController.cs
+++ Webion.Stargaze.Api/Controllers/v1/Accounting/Invoices/Items/GetAll/GetAllInvoiceItemsController.cs
@@ -1,6 +1,49 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Webion.Stargaze.Pgsql;
+
 namespace Webion.Stargaze.Api.Controllers.v1.Accounting.Invoices.Items.GetAll;
 
-public class GetAllInvoiceItemsController
+[Authorize]
+[ApiController]
+[Route("v{version:apiVersion}/invoices/{invoiceId}/items")]
+[Tags("Invoices")]
+[ApiVersion("1.0")]
+public sealed class GetAllInvoiceItemsController : ControllerBase
 {
-    
+    private readonly StargazeDbContext _db;
+
+    public GetAllInvoiceItemsController(StargazeDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    [ProducesResponseType<GetAllInvoiceItemsResponse>(200)]
+    public async Task<IActionResult> GetAll(
+        [FromRoute] Guid invoiceId,
+        CancellationToken cancellationToken
+    )
+    {
+        var invoices = await _db.InvoiceItems
+            .Where(x => x.InvoiceId == invoiceId)
+            .Select(x => new GetAllInvoiceItemsResponse
+            {
+                Id = x.Id,
+                InvoiceId = x.InvoiceId,
+                TotalUnits = x.TotalUnits,
+                Description = x.Description,
+                NetPrice = x.NetPrice,
+                TaxedPrice = x.TaxedPrice,
+                VatPercentage = x.VatPercentage
+            })
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+
+        if (invoices is null)
+            return NotFound();
+
+        return Ok(invoices);
+    }
 }
