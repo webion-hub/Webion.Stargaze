c60f849e2d799ac25d561f33287b15a6abf64fff
Stefano Calabretti <stefano.calabretti@webion.it>
2024-05-18 18:53:34 +0200

    fix: Refactor ExchangeCodeController and cleanup
    This commit simplifies the ExchangeCodeController class and updates access control. Unused dependencies including StargazeDbContext and ILogger have been removed. Also, the controller now allows anonymous access for its functionality. Furthermore, a minor change was made in CreateClientResult in the Clients namespace.

--- Webion.Stargaze.Api/Controllers/v1/Connect/Token/Exchange/ExchangeCodeController.cs
+++ Webion.Stargaze.Api/Controllers/v1/Connect/Token/Exchange/ExchangeCodeController.cs
@@ -1,29 +1,23 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Webion.Stargaze.Auth.Services.Clients;
 using Webion.Stargaze.Auth.Services.Jwt.Exchange;
-using Webion.Stargaze.Pgsql;
 
 namespace Webion.Stargaze.Api.Controllers.v1.Connect.Token.Exchange;
 
-[Authorize]
+[AllowAnonymous]
 [ApiController]
 [Route("v{version:apiVersion}/connect/token/exchange/{code}")]
 [Tags("Connect")]
 [ApiVersion("1.0")]
 public sealed class ExchangeCodeController : ControllerBase
 {
-    private readonly StargazeDbContext _db;
     private readonly IClientsManager _clientsManager;
     private readonly IExchangeCodeManager _exchangeCodeManager;
-    private readonly ILogger<ExchangeCodeController> _logger;
 
-    public ExchangeCodeController(StargazeDbContext db, IExchangeCodeManager exchangeCodeManager, ILogger<ExchangeCodeController> logger, IClientsManager clientsManager)
+    public ExchangeCodeController(IExchangeCodeManager exchangeCodeManager, IClientsManager clientsManager)
     {
-        _db = db;
         _exchangeCodeManager = exchangeCodeManager;
-        _logger = logger;
         _clientsManager = clientsManager;
     }
     
@@ -36,10 +30,6 @@ public sealed class ExchangeCodeController : ControllerBase
         CancellationToken cancellationToken
     )
     {
-        var client = await _db.Clients
-            .Where(x => x.Id == request.ClientId)
-            .FirstOrDefaultAsync(cancellationToken);
-
         var isValid = await _clientsManager.VerifyAsync(
             id: request.ClientId,
             base64Secret: request.ClientSecret,
--- Webion.Stargaze.Auth/Services/Clients/CreateClientResult.cs
+++ Webion.Stargaze.Auth/Services/Clients/CreateClientResult.cs
@@ -3,4 +3,3 @@ using Webion.Stargaze.Pgsql.Entities.Connect;
 namespace Webion.Stargaze.Auth.Services.Clients;
 
 public sealed record CreateClientResult(ClientDbo Client, string PlainTextSecret);
-
