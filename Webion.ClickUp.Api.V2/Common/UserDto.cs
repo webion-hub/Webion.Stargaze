7f338056cdc0d92aa73f2571f545fa8713777c6f
Filippo Bellei <filippobellei@gmail.com>
2024-05-23 17:03:23 +0200

    fix(WEBION-95): SyncClickUpUsers fixes

--- Webion.ClickUp.Api.V2/Common/UserDto.cs
+++ Webion.ClickUp.Api.V2/Common/UserDto.cs
@@ -5,11 +5,12 @@ namespace Webion.ClickUp.Api.V2.Common;
 public sealed class UserDto
 {
     public required ClickUpId Id { get; init; }
-    
+
     [JsonPropertyName("username")]
     public required string UserName { get; init; }
     public required string Color { get; init; }
-    
+
     [JsonPropertyName("profilePicture")]
     public required string ProfilePicture { get; init; }
+    public required string Email { get; init; }
 }
