2db4bcd05e4e2acfecdda7999ddf0c9ed795fe34
Filippo Bellei <filippobellei@gmail.com>
2024-05-23 10:37:19 +0200

    fix(WEBION-163): add access token claim

--- Webion.AspNetCore.Authentication.ClickUp/ClickUpClaims.cs
+++ Webion.AspNetCore.Authentication.ClickUp/ClickUpClaims.cs
@@ -2,7 +2,7 @@ namespace Webion.AspNetCore.Authentication.ClickUp;
 
 public static class ClickUpClaims
 {
-    public const string AccessToken = "access_token";
+    public const string AccessToken = "AccessToken";
     public const string ProfilePicture = "profile_picture";
     public const string Color = "color";
 }
