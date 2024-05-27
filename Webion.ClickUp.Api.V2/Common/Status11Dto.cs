3439c5e1beb8a4bd8ac1a0384a6334cb3714f701
Filippo Bellei <filippobellei@gmail.com>
2024-05-27 10:30:24 +0200

    fix(WEBION-170): fixes ClickUpObjectsSynchronizer dtos

--- Webion.ClickUp.Api.V2/Common/Status11Dto.cs
+++ Webion.ClickUp.Api.V2/Common/Status11Dto.cs
@@ -4,11 +4,11 @@ namespace Webion.ClickUp.Api.V2.Common;
 
 public sealed class Status11Dto
 {
-    public required string Id { get; init; }
+    public string? Id { get; init; }
     public required string Status { get; init; }
     public required string Color { get; init; }
 
     [JsonPropertyName("orderindex")]
     public int OrderIndex { get; init; }
-    public required string Type { get; init; }
+    public string? Type { get; init; }
 }
