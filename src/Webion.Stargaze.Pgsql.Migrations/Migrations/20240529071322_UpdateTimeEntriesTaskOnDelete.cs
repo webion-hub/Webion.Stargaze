f4967be15eb135fd5309a5986d29f25fb628baa0
Filippo Bellei <filippobellei@gmail.com>
2024-05-29 09:25:43 +0200

    feat(WEBION-94): update Task OnDelete in TimeEntryDbo

---
+++ src/Webion.Stargaze.Pgsql.Migrations/Migrations/20240529071322_UpdateTimeEntriesTaskOnDelete.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Webion.Stargaze.Pgsql.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTimeEntriesTaskOnDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_time_entry_task_task_id",
+                schema: "time_tracking",
+                table: "time_entry");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_time_entry_task_task_id",
+                schema: "time_tracking",
+                table: "time_entry",
+                column: "task_id",
+                principalSchema: "projects",
+                principalTable: "task",
+                principalColumn: "id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_time_entry_task_task_id",
+                schema: "time_tracking",
+                table: "time_entry");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_time_entry_task_task_id",
+                schema: "time_tracking",
+                table: "time_entry",
+                column: "task_id",
+                principalSchema: "projects",
+                principalTable: "task",
+                principalColumn: "id");
+        }
+    }
+}
