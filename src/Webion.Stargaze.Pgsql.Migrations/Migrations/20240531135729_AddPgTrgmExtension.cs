1a554f6f39b58c4f10e0699a1a3bdc10fcf91d43
Filippo Bellei <filippobellei@gmail.com>
2024-05-31 15:58:29 +0200

    fix(WEBION-102): add pg_trgm migrations

---
+++ src/Webion.Stargaze.Pgsql.Migrations/Migrations/20240531135729_AddPgTrgmExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Webion.Stargaze.Pgsql.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPgTrgmExtension : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE EXTENSION pg_trgm;");
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP EXTENSION pg_trgm;");
+        }
+    }
+}
